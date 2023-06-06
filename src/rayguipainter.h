b5ef036578f6dd9de9dda8cc8255847bc5eb9f9c
mdavisprog <mdavisprog@gmail.com>
2023-06-06 11:54:09 -0700

    Label painting.
    A new function 'guiPainterLabel' has been introduced to draw a label through
    the 'GuiLabel' function and will automatically advance the cursor to the next
    line. Labels and future controls will also have spacing applied for the next
    control.

--- src/rayguipainter.h
+++ src/rayguipainter.h
@@ -12,6 +12,7 @@ extern "C" {
 void GuiPainterSetCursorPos(Vector2 pos);
 
 bool GuiPainterWindowBox(Vector2 size, const char* title);
+void GuiPainterLabel(const char* text);
 
 #if defined(__cplusplus)
 }
@@ -23,6 +24,12 @@ bool GuiPainterWindowBox(Vector2 size, const char* title);
 
 static Vector2 guiPainterCursorPos = { 0.0f, 0.0f };
 static Vector2 guiPainterCursorSize = { 0.0f, 0.0f };
+static Vector2 guiPainterControlSpacing = { 4.0f, 4.0f };
+
+static Vector2 GuiPainterTextSize(const char* text)
+{
+    return MeasureTextEx(GuiGetFont(), text, (float)GuiGetStyle(DEFAULT, TEXT_SIZE), (float)GuiGetStyle(DEFAULT, TEXT_SPACING));
+}
 
 void GuiPainterSetCursorPos(Vector2 pos)
 {
@@ -33,9 +40,17 @@ bool GuiPainterWindowBox(Vector2 size, const char* title)
 {
     const Rectangle bounds = { guiPainterCursorPos.x, guiPainterCursorPos.y, size.x, size.y };
     bool result = GuiWindowBox(bounds, title);
-    guiPainterCursorPos.y += RAYGUI_WINDOWBOX_STATUSBAR_HEIGHT;
+    guiPainterCursorPos.y += RAYGUI_WINDOWBOX_STATUSBAR_HEIGHT + guiPainterControlSpacing.y;
     guiPainterCursorSize = size;
     return result;
+}
+
+void GuiPainterLabel(const char* text)
+{
+    const Vector2 textSize = GuiPainterTextSize(text);
+    const Rectangle bounds = { guiPainterCursorPos.x + guiPainterControlSpacing.x, guiPainterCursorPos.y, textSize.x + 6.0f, textSize.y };
+    GuiLabel(bounds, text);
+    guiPainterCursorPos.y += bounds.height + guiPainterControlSpacing.y;
 }
 
 #endif // RAYGUIPAINTER_IMPLEMENTATION
