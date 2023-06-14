88d5310600d73f448092aadcdd7fc1705feec920
mdavisprog <mdavisprog@gmail.com>
2023-06-13 20:50:43 -0700

    Add example usage of 'rayguipainter' API functions.
    This includes functions that are currently implemented in the library.

--- examples/showcase/main.c
+++ examples/showcase/main.c
@@ -1,14 +1,39 @@
 #include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#define RAYGUIPAINTER_IMPLEMENTATION
+#include "rayguipainter.h"
 
 int main()
 {
-    InitWindow(960, 540, "Showcase");
+    const int WindowWidth = 960;
+    const int WindowHeight = 540;
+
+    InitWindow(WindowWidth, WindowHeight, "Showcase");
     SetTargetFPS(60);
+
+    const Vector2 CursorPos = { 0.0f, 0.0f };
+    const Vector2 WindowBoxSize = { (float)WindowWidth, (float)WindowHeight };
+
+    bool Toggle = false;
+    char TextBox[255] =  { 0 };
+    bool TextBoxEdit = false;
+    GuiPainterDropdownBoxOptions DropdownBoxOptions = { 0 };
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        GuiPainterSetCursorPos(CursorPos);
+        GuiPainterWindowBox(WindowBoxSize, "Controls");
+
+        GuiPainterLabel("Label");
+        GuiPainterButton("Button");
+        GuiPainterLabelButton("Label Button");
+        Toggle = GuiPainterToggle("Toggle", &Toggle);
+        GuiPainterTextBox(TextBox, sizeof(TextBox), &TextBoxEdit);
+        GuiPainterDropdownBox("Red;Green;Blue", &DropdownBoxOptions);
+
         EndDrawing();
     }
 
